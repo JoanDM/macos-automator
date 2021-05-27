d83bdea0140de587dfc21fdcf4bad6c50c6f3aa6
Joan Domènech <joan.domenech.masferrer@gmail.com>
2021-05-27 21:02:48 +0200

    Add files via upload

--- automator_class.py
+++ automator_class.py
@@ -3,6 +3,7 @@ from pynput.keyboard import Controller as KeyboardController
 from pynput.mouse import Button
 from pynput.keyboard import Key
 from datetime import datetime
+import subprocess
 
 
 class Automator(object):
@@ -18,11 +19,11 @@ class Automator(object):
             current_time = datetime.now()
             time_diff = (current_time - previous_time).total_seconds()
 
-    def click(self, mouse_position):
+    def click(self, mouse_position, idle_time=0.05):
         self.mouse.position = mouse_position
-        self.idle_time(1)
+        self.idle_time(idle_time)
         self.mouse.click(Button.left, 1)
-        self.idle_time(1)
+        self.idle_time(idle_time)
 
     def double_click(self, mouse_position):
         self.mouse.position = mouse_position
@@ -35,6 +36,20 @@ class Automator(object):
         self.idle_time(1)
         self.mouse.click(Button.left, 3)
         self.idle_time(1)
+
+    def press_cmd_tab(self, idle_time=0.2):
+        self.keyboard.press(Key.cmd)
+        self.keyboard.press(Key.tab)
+        self.keyboard.release(Key.cmd)
+        self.keyboard.release(Key.tab)
+        self.idle_time(idle_time)
+
+    def refresh(self, idle_time=1.5):
+        self.keyboard.press(Key.cmd)
+        self.keyboard.press("r")
+        self.keyboard.release(Key.cmd)
+        self.keyboard.release("r")
+        self.idle_time(idle_time)
 
     def search_spotlight_and_launch(self, text):
         with self.keyboard.pressed(Key.cmd):
@@ -58,15 +73,19 @@ class Automator(object):
         self.keyboard.release(Key.shift)
         self.idle_time(4)
 
-    def screenshot_keyboard_shortcut(self):
+    def screenshot_keyboard_shortcut(self, idle_time=4):
         self.keyboard.press(Key.cmd)
         self.keyboard.press(Key.shift)
-        self.idle_time(1)
         self.keyboard.press("3")
         self.keyboard.release("3")
         self.keyboard.release(Key.cmd)
         self.keyboard.release(Key.shift)
-        self.idle_time(4)
+        self.idle_time(idle_time)
+
+    def take_screenshot_with_coordinates(self, top_x, left_y, width, heigth, filename):
+        subprocess.check_output(
+            ["screencapture", f"-R{top_x},{left_y},{width},{heigth}", f"{filename}.png"]
+        )
 
     def type(self, text):
         self.keyboard.type(text)
