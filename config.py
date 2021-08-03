66a49b3ac6e1a7a8ba00eb95abfbf9479f86aa65
Joan Domènech <joan.domenech.masferrer@gmail.com>
2021-08-03 20:05:27 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ _base_dir = os.path.abspath(os.path.dirname(__file__))
 _base_dir_pathlib = Path(_base_dir)
 _resources_dir_pathlib = _base_dir_pathlib / "resources"
 _stored_mouse_positions_dir_pathlib = _resources_dir_pathlib / "stored_mouse_positions"
+_chromedriver_pathlib = _resources_dir_pathlib / "chromedriver"
 
 
 def create_valid_file_path(target_file_path):
